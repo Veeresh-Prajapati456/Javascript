ac41c4e0898dec5b64268e2e3d2e738a059c9214
Veeresh Prajapati <121452871+Veeresh-Prajapati456@users.noreply.github.com>
2024-06-16 14:00:34 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "veeresh@gmail.com"
+var accountPassword = "v4455"
+let accountState;
+
+city = "Punjab"  //we can also declare variable without using type of variable like const, let etc but this is the bad practice.
+
+console.log(accountId);
+console.log(city);
+
+
+accountPassword = "kp456"
+city = "Delhi"
+
+/* To print Data in Tabular Form then, use console.table([varname1, varname2, varname3]) */
+console.table([accountId, accountEmail, accountPassword, accountState, city])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,45 @@
+"use strict";  //treat all JS code as newer version of Javascript
+
+//alert(3+3)  //we are using nodejs, not browser because in browser "console" if we write- alert(3+3) then pop will show with the addition of given number that is- 6
+
+console.log(3+7); //it will print 10
+
+
+//Primitive/ In-built DataTypes
+
+let name = "Aman"
+let age = 18
+let isLoggedIn = false
+let state = null
+let city;
+
+
+//Primitive/ In-built DataTypes
+
+/* 
+number--- 2 to the power 53 approx
+
+bigint
+
+string
+
+boolean
+
+null-----empty value(standalone value)
+
+undefined----value is not assign
+
+Symbol----unique(Example--used in React to make unique components, also used in figma to define )individual or unique component)
+
+*/
+
+
+//to check the type of variable 
+console.log(typeof age);  ///it will return the type of (age) as "number" because we declare number to the age variable
+/*          OR          */
+console.log(typeof "Veeresh");
+
+
+console.log(typeof null);  //it will return the typeof (null) as object
+
+console.log(typeof undefined);  //it will return the typeof (undefined) as undefined
