d5440003f1d265c290ff188aac67f1bc8abb7c17
Veeresh Prajapati <121452871+Veeresh-Prajapati456@users.noreply.github.com>
2024-07-30 13:47:31 +0000

    comparision update

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,14 +5,14 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 // console.log("02" < 1);
-console.log("03" <= true); //false
-console.log(1 > false); //true
-console.log(2 < false); //false
-console.log(1 >= true); //true
-console.log("" > true); //false
-console.log("" > false); //false
-console.log("" < true); // true
-console.log("" < false); //false
+// console.log("03" <= true); //false
+// console.log(1 > false); //true
+// console.log(2 < false); //false
+// console.log(1 >= true); //true
+// console.log("" > true); //false
+// console.log("" > false); //false
+// console.log("" < true); // true
+// console.log("" < false); //false 
 
 
 
@@ -22,11 +22,14 @@ console.log("" < false); //false
 // console.log(null <= 0); //true
 // console.log(null < 0); //false
 
-/* The reason is that an equality check == and comparisions > < >= <= work differently.
+/* The reason is that an equality check == and comparisions >(greaterthan) <(lessthan) >= <= work differently.
 Comparisions convert null to a number, treating it as 0.
-That's why line no. (12 and 13) null >= 0 , null <= 0 is true, and line no. (10, 11, and 14) null > 0, null == 0, and null < 0 is false  */
+That's why line no. (21 and 22) null >= 0 , null <= 0 is true, and line no. (19, 20, and 23) null > 0, null == 0, and null < 0 is false  */
 
 
+
+console.log("2" === 2);
+console.log(null === 0);
 
 
 
