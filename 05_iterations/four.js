0ec7734e44529339d1166df585f56e9807cd41e1
Veeresh Prajapati <121452871+Veeresh-Prajapati456@users.noreply.github.com>
2024-08-29 09:08:48 +0000

    four js done

---
+++ 05_iterations/four.js
@@ -0,0 +1,61 @@
+// Forin loop is work with Objects
+
+// Object
+
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'c++',
+//     rb: 'ruby',
+//     swift: 'swift by apple'
+// }
+
+// for (const key in myObject) {
+    // console.log(key);   //Returns only keys.
+
+    // console.log(myObject[key]);   //Returns only values
+    
+
+    // console.log(`${key} shortcut is for ${myObject[key]}`);     //Returns key-value pair
+     
+// }
+
+
+
+// Array
+// Forof Loop
+
+// const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const [key,value] of programming) {
+//     // console.log(key);  //Return only values
+    
+// }
+
+
+
+// Forin Loop
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log(key);  //Return only keys
+    
+    // console.log(programming[key]);  //Returns only Values
+    
+}
+
+// *******************************************************************
+
+
+// Map with hep of forin Loop
+// Mp is not return any output with Forin Loop, because map is not iterable in case of Forin
+
+// const map = new Map()
+// map.set('IND', "India")
+// map.set('USA', "United state of America")
+// map.set('JPN', "Japan")
+
+// for (const key in map) {
+//     console.log(key);   
+
+// }
