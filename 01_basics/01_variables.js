cc0edf0875548d424cf7617009279ad59df5fc9c
Veeresh Prajapati <121452871+Veeresh-Prajapati456@users.noreply.github.com>
2024-10-16 18:13:35 +0000

    done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 144553
 let accountEmail = "veeresh@gmail.com"
-var accountPassword = "v4455"
-let accountState;
+var accountPassword = "v4455" 
+let accountState;   //if we are not define the value of a variable, then it will return "Undefined"
 
 city = "Punjab"  //we can also declare variable without using type of variable like const, let etc but this is the bad practice.
 
@@ -15,3 +15,6 @@ city = "Delhi"
 /* To print Data in Tabular Form then, use console.table([varname1, varname2, varname3]) */
 console.table([accountId, accountEmail, accountPassword, accountState, city])
  
+/* Prefer not to use var 
+Because of issue in block scope and functional scope 
+*/
