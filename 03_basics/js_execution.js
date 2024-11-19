627c29bd6c61c11aca3dd9855247b154240de3fd
Veeresh Prajapati <121452871+Veeresh-Prajapati456@users.noreply.github.com>
2024-11-19 15:35:11 +0000

    done

--- 03_basics/js_execution.js
+++ 03_basics/js_execution.js
@@ -1,17 +1,25 @@
 //"Javascript Execution Context" means how JavaScript run/execute whatever file you have create?
+
 // To run/ Execute the program, JavaScript runs/Execute the program in two Phases.
+
 //When we give the code file to the Javascript, first thing is always created that is "Global Execution Context"
-//and wherever the global Execution Context will be created it will be referred to "this" variable, and our "Global Execution context" will be kept under "this" variable.
+
+//and wherever the global Execution Context will be created it will be referred to "this" variable, and our "Global Execution context" will be kept in "this" variable.
 
 
 // To run/ Execute the program, JavaScript runs/Execute the program in two Phases.
+
 //  1) Global Execution Context
+
 //  2)Function/Functional Execution Context
         //Every time a function is executed, a new execution context is created for that function called local execution context or function execution context.
+
 //  3)Eval Execution Context (optional) (This is a property of Global execution context in itself)
+
 
 // Important Note:-
 //JavaScript is a single threaded  ( means just one line of code may be run at once) Language
+
 
 // Imp. Interview Question:
 //In Browser the Value of "this" while Global Execution Context is "Window Object"
@@ -19,6 +27,7 @@
 
 
 // Let's suppose we have JavaScript Code and the code will run in two phases.
+
 // 1) Memory Creation Phase/Creation Phase:- In the Memory Creation phase only memory is allocated to the variables and functions. For all variables value stored in memory is undefined and functions are copied in memory
 
 // 2) Execution Phase:-  JavaScript code is executed line by line and it is defining variables in memory here.
@@ -38,6 +47,7 @@ console.log(result2);
 //First Cycle Of Every Program , it will done in every program.
 
 //step1:- Global Execution/Global Environment (First of all, whatever code has to be run is run from the global executive and it is allocated first inside "this".)   OR   (sab se phele jo bhi code run Karna ho wo global execution se run hota hai aur iss ko sabse phele allocate kiya jata hai "this"ke andar)
+
 //Step2:- Memory Creation Phase/ Memory Phase (In the memory creation phase, all the variables are collected and kept in memory.)
 //          val1 => undefined
 //          val2 => undefined
