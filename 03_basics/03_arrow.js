a182ef7379ba3872e98cbc462b128e93d81f97d1
Veeresh Prajapati <121452871+Veeresh-Prajapati456@users.noreply.github.com>
2024-11-19 15:34:54 +0000

    done

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,7 +1,7 @@
 //Important Interview Question
 //What is the Globel Object in Browser?
 //Answer:-  Window Object
-//this:- means what Current context(current values/variables) hold.
+//this:- means, refers to the Current context(current values/variables) hold.
 
 const user = {
     username: "veeresh",
@@ -12,7 +12,9 @@ const user = {
         console.log(this);
         
     }
+
 }
+
 
 // user.welcomeMessage()      // Return:- veeresh, welcome to website
                               //          {
@@ -22,7 +24,7 @@ const user = {
                               //          }
                                                         
 
-// user.username = "Ankit" //Here current context is changed from "Veeresh" to "Ankit", contect means what values hold variable is?
+// user.username = "Ankit" //Here current context is changed from "Veeresh" to "Ankit", context means what values hold variable is?
 // user.welcomeMessage()      // Return:-  Ankit, welcome to website
                               //          {
                               //            username: 'Ankit',
@@ -32,7 +34,7 @@ const user = {
                              
 
 
-// console.log(this); //Return:- Empty Object:-  {} because there is no context in global scope.
+console.log(this); //Return:- it will return {} ( Empty Object ) because there is no context in the global scope.
 
 
 
@@ -62,7 +64,7 @@ const user = {
 
 // const chai = function(){
 //   let username = "veeresh"
-//   console.log(this.username);
+  // console.log(this.username); //Return:- Undefined, because we can not use ( this.username ) (username:- current context) inside the function, it will not work in function.  // this .current_context work in object
   
 // }
 // chai()
@@ -102,8 +104,8 @@ chai()
 
 
 // Arrow Function
-// This way is called "Explicit Return" because we use "return" in it means (" return" alag se lagana pad raha hai)
-// const addTwo = (num1, num2) => {    //If we use Curly Braces the we have to use "return" in the below line
+// This way is called "Explicit Return" because we additionally use "return" in it, means (" return" alag se lagana pad raha hai)
+// const addTwo = (num1, num2) => {    //If we use Curly Braces then we have to use "return" in the below line
 //   return num1 + num2
 // }
 // console.log(addTwo(3, 5));  //Return:- 8
@@ -128,9 +130,12 @@ chai()
 
 
 // Benefits of using "Implicit Return" or Parenthecis.:- Arrow Function
-const addTwo = (num1, num2) => ({username : "veeresh"})   //Return:- { username: 'veeresh' }  //If we want to return Object we need to wrap in parenthecis because we will not wrap in parenthecis then then it will return:- "undefined" Eg:- const addTwo = (num1, num2) => {username:"veeresh"} it will return undefined, because we can't return object in function in this way
+// Implicit return is useful for creating succinct one-line operations in map , filter , and other common array methods.
 
-console.log(addTwo(3, 5));  
+
+// const addTwo = (num1, num2) => ({username : "veeresh"})   //Return:- { username: 'veeresh' }  //If we want to return Object through Arrow Function then we need to wrap in parenthecis because if we will not wrap in parenthecis then it will return:- "undefined" Eg:- const addTwo = (num1, num2) => {username:"veeresh"} it will return undefined, because if we want to return the object in Arrow function then we need to use " ({}) "
+
+// console.log(addTwo(3, 5));  
 
 
 
