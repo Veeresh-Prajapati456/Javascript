299ea68204998f8f97a1ca54d1d1278307ff3a79
Veeresh Prajapati <121452871+Veeresh-Prajapati456@users.noreply.github.com>
2024-11-24 14:35:57 +0000

    done

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -1,12 +1,12 @@
-// const myNums = [1, 2, 3, 4]
+const myNums = [1, 2, 3, 4]
 
-// const myTotal = myNums.reduce(function (accumulator, currentvalue) {
-//     console.log(`acc: ${accumulator} and currentvalue: ${currentvalue}`);
+const myTotal = myNums.reduce(function (accumulator, currentvalue) {
+    console.log(`acc: ${accumulator} and currentvalue: ${currentvalue}`);
     
-//     return accumulator + currentvalue
-// }, 0) //( 0  is the accumulator value in the starting, we can give any value to the accumulator)
+    return accumulator + currentvalue
+}, 0)      //( 0  is the accumulator value in the starting, we can give any value to the accumulator)
 
-// console.log(myTotal);
+console.log(myTotal);
 // return:-
 // acc: 0 and currentvalue: 1
 // acc: 1 and currentvalue: 2
@@ -18,12 +18,12 @@
 
 //Solving above problem with Arrow function
 
-const myNums = [1, 2, 3, 4]
+// const myNums = [1, 2, 3, 4]
 
-const myTotal = myNums.reduce( (acc, currval) => acc + currval, 0)
+// const myTotal = myNums.reduce( (acc, currval) => acc + currval, 0)
 //( 0  is the accumulator value in the starting, we can give any value to the accumulator)
 
-console.log(myTotal);
+// console.log(myTotal);
 
 // **********************************************************
 
